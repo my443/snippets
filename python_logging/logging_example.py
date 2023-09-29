f9769345420b887bbc6304394b8ff3fde1fd1956
John Van Dijk <fortyfour-three@teksavvy.com>
2023-09-29 15:24:08 -0400

    Python logging example.

---
+++ python_logging/logging_example.py
@@ -0,0 +1,18 @@
+import logging
+
+FORMAT = "%(levelname)-8s %(asctime)s %(message)s"
+dateformat = '%Y-%m-%d %H:%M:%S'
+
+
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG, format=FORMAT, datefmt=dateformat)
+
+for i in range(10):
+    logging.debug('This message should go to the log file')
+    logging.info('This message should go to the log file')
+    logging.warning('This message should go to the log file')
+    logging.error('This message should go to the log file')
+
+
+## Learning came from:
+## https://stackoverflow.com/questions/7771912/how-to-right-align-level-field-in-python-logging-formatter
+## https://stackoverflow.com/questions/3220284/how-to-customize-the-time-format-for-python-logging

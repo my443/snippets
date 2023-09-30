14522cb4c1a0ba80603b4f6794b75eb088c7e1e9
John van Dijk <fortyfour-three@teksavvy.com>
2023-09-30 07:01:32 -0400

    Saving progress on the pairwise problem.

--- javascript/pairwise/pairwise.js
+++ javascript/pairwise/pairwise.js
@@ -1,15 +1,47 @@
 function pairwise(arr, arg) {
-    return arg;
+    array_to_sum = check(arr, arg);
+    // console.log (array_to_sum);
+    return sum(array_to_sum);
+    // return arg;
+}
+
+function find_number_in_array(arr, num) {
+    index_number = arr.findIndex((e) => e == num);
+    return index_number;
+}
+
+function check(arr, num) {
+    used_indexes = [];
+
+    console.log('index', '|  where the number exists')
+
+    arr.forEach((element, index) => {
+        number_to_look_for = num - element;
+        number_exists = find_number_in_array(arr, number_to_look_for);
+
+        if (number_exists == -1) {
+            return false;
+        }
+        else {
+            console.log(' ', index, '     ', number_exists);
+            if (find_number_in_array(used_indexes, index) == -1 && find_number_in_array(used_indexes, number_exists) == -1) {
+                used_indexes.push(index);
+                used_indexes.push(number_exists);
+            }
+        }
+        console.log(used_indexes);        
+    });
+    return used_indexes;
 }
 
 function sum(indexesArray) {
     /* Takes a flat array of numbers. 
-        Returns the sum of those numbers. */ 
-    
-        returnValue = indexesArray.reduce((accumulator, currentValue) =>
+        Returns the sum of those numbers. */
+    console.log(indexesArray);
+    returnValue = indexesArray.reduce((accumulator, currentValue) =>
         accumulator + currentValue, 0
     )
-    
+
     return returnValue
 }
 
@@ -17,7 +49,13 @@ function sum(indexesArray) {
 //   console.log(x)
 
 
-module.exports = {pairwise, sum};
+// check([5, 6, 4, 2, 5, 7, 1], 6);
+// check([7, 9, 11, 13, 15], 20);
+// n = pairwise([7, 9, 11, 13, 15], 20);
+// console.log(n);
+check([1, 1, 1], 2)
+
+module.exports = { pairwise, sum, find_number_in_array };
 
 // export {sum};
 
--- javascript/pairwise/pairwise.test.js
+++ javascript/pairwise/pairwise.test.js
@@ -1,9 +1,29 @@
 
 // const pairwise = require('./pairwise');
-const { pairwise, sum } = require('./pairwise');
+const { pairwise, sum, find_number_in_array } = require('./pairwise');
 
-test('Test an array with many numbers', () => {
-    expect(pairwise([1, 4, 2, 3, 0, 5], 7)).toBe(7);
+test('Test an array with many numbers 1', () => {
+    expect(pairwise([1, 4, 2, 3, 0, 5], 7)).toBe(11);
+});
+
+test('Test an array with many numbers 2', () => {
+    expect(pairwise([7, 9, 11, 13, 15], 20)).toBe(6);
+});
+
+test('Test an array with many numbers 3', () => {
+    expect(pairwise([1, 3, 2, 4], 4)).toBe(1);
+});
+
+test('Test an array with many numbers 4', () => {
+    expect(pairwise([1, 1, 1], 2)).toBe(1);
+});
+
+test('Test an array with many numbers 5', () => {
+    expect(pairwise([0, 0, 0, 0, 1, 1], 1)).toBe(10);
+});
+
+test('Test an array with many numbers 6', () => {
+    expect(pairwise([], 100)).toBe(0);
 });
 
 //   Testing sum
@@ -22,4 +42,18 @@ test("Sum array with negative", () => {
 test("Sum larger array numbers", () => {
     expect(sum([50000, 10, 12312312412])).toBe(12312362422)
 })
+
+// Testing find function
+test("Find with a value that exists", () => {
+    expect(find_number_in_array([1, 5, 3, 7, 2, 2], 5)).toBe(1)
+})
+
+test("Find where doesn't exist", () => {
+    expect(find_number_in_array([1, 5, 3, 7, 2, 2], 8)).toBe(-1)
+})
+
+test("Find with two of the same number", () => {
+    expect(find_number_in_array([1, 5, 3, 7, 5, 2], 5)).toBe(1)
+})
+
 

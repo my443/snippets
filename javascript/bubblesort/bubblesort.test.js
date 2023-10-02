b4dd46dbf1a9ec5fc13db88e6dfba139dbf26868
John van Dijk <fortyfour-three@teksavvy.com>
2023-10-02 06:34:59 -0400

    javascript bubblesort

--- javascript/bubblesort/bubblesort.test.js
+++ javascript/bubblesort/bubblesort.test.js
@@ -1,1 +1,13 @@
 const { bubbleSort } = require('./index');
+
+test('Test a sort 1', () => {
+    expect(bubbleSort([1, 7, 3, 2, 8, 5])).toStrictEqual([1, 2, 3, 5, 7, 8]);
+});
+
+test('Test an array with many numbers 1', () => {
+    expect(bubbleSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92])).toStrictEqual([1, 1,  2, 2,    4,  8, 32,43, 43, 55, 63, 92, 123, 123, 234, 345, 5643]);
+});
+
+test('Test an empty array', () => {
+    expect(bubbleSort([])).toStrictEqual([]);
+});
--- javascript/bubblesort/index.js
+++ javascript/bubblesort/index.js
@@ -1,7 +1,23 @@
 function bubbleSort(array) {
     // Only change code below this line
+    
+
+    array.every((element, index) => {
+        let continuevar= true;
+        for (let i = 0; i < array.length- index - 1; i++){
+            if (array[i] > array[i+1]) {
+                [array[i+1], array[i]] = [array[i], array[i+1]];
+                continuevar == false;
+            }
+        }
+        return continuevar;
+    });
+    
     return array;
     // Only change code above this line
   }
+
+
+console.log(bubbleSort([1, 7, 3, 2, 8, 5]));
 
   module.exports = { bubbleSort };

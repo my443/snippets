c551763db35b1348c6aa542661432364f47a3715
John van Dijk <fortyfour-three@teksavvy.com>
2023-10-23 06:58:37 -0400

    Looking at deploying java.

---
+++ java/simple_calculator_java/Calculate.java
@@ -0,0 +1,19 @@
+
+// package Calculate.java;
+import javax.swing.*;
+        
+
+/**
+ *
+ * @author john
+ */
+public class Calculate {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is working. \nHello World!");
+    }
+    
+}
---
+++ java/simple_calculator_java/Simple.java
@@ -0,0 +1,8 @@
+// https://www.javatpoint.com/simple-program-of-java
+// To test Java compilation
+
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  

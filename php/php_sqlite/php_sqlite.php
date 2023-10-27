9be2e4c968eb2878c69cbd9d02ab838bf9231f47
John Van Dijk <fortyfour-three@teksavvy.com>
2023-10-27 12:04:33 -0400

    Database object created.

--- php/php_sqlite/php_sqlite.php
+++ php/php_sqlite/php_sqlite.php
@@ -1,3 +1,20 @@
 <?php 
-$database = new SQLite3('mydb.db');
+echo "hello\n\n";
+
+
+
+
+class Database{
+
+    function create_main_table(){
+        //
+    }
+
+    public function __construct(){
+        $database = new SQLite3('mydb.db');
+    }
+}
+
+$db = new Database;
+
 ?>

15fc6b33d1be2a14901e6d4564dc6646b4d8f796
John van Dijk <fortyfour-three@teksavvy.com>
2023-11-06 06:42:58 -0500

    Added code for the OOP presentation.

--- oop_demo/oop.js
+++ oop_demo/oop.js
@@ -9,6 +9,9 @@ class Car{
 
 class Car{
 
+    type = 'Ford';
+    driver = null;
+
     drive(){
         if (this.driver == null){
             console.log('There is no driver in the car.');
@@ -20,6 +23,51 @@ class Car{
 
     constructor(type, driver) {
         this.type = type;
+    }
+
+}
+
+// #0 
+class Person{
+    name = '';
+    age = 0;
+    has_drivers_license = false;
+
+}
+
+// #1
+class Person{
+    name = '';
+    age = 0;
+    has_drivers_license = false;
+
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+// #2
+class Car{
+    manufacturer = null;
+    driver = null;
+
+    drive(){
+        console.log('Car is driving forward.');
+    }
+}
+
+// #3
+class Car{
+    manufacturer = null;
+    driver = null;
+
+    drive(){
+        console.log('Car is driving forward.');
+    }
+
+    constructor(manufacturer) {
+        this.manufacturer = type;
     }
 
 }

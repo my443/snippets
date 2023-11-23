0ce1c62b2a0d665ffd0543689985062f5d401d4b
John van Dijk <fortyfour-three@teksavvy.com>
2023-11-22 22:33:07 -0500

    Fibonacci works completely.

--- algorithms/python/Fibonacci/fibonacci_full.py
+++ algorithms/python/Fibonacci/fibonacci_full.py
@@ -1,5 +1,15 @@
+#python3
+def calc_fib(n):
+    returnValue = 0
 
+    if n != 0:
+        f = [0, 1]
+        for i in range(n-1):
+            f.append(f[i] + f[i+1])
 
-f = [0, 1]
-for i in range(10):
-    f.append(f[i] + f[i+1])
+        returnValue = f[-1]
+
+    return returnValue
+
+n = int(input())
+print(calc_fib(n))
